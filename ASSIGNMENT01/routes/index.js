14698d88cc94261e91fb8cc875885463bb334211
HastiDonga25 <146013279+HastiDonga25@users.noreply.github.com>
2024-10-18 13:17:12 -0400

    creatred major part of portfolio

--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/portfolio', async(req, res, next) => {
+  res.render('portfolio/index', { title: 'Portfolio' });
+});
+
+router.get('/portfolio/about', async(req, res, next) => {
+  res.render('portfolio/about', { title: 'ABOUT' });
+});
+
+router.get('/portfolio/services', async(req, res, next) => {
+  res.render('portfolio/services', { title: 'ABOUT' });
+});
+
+router.get('/portfolio/contact', async(req, res, next) => {
+  res.render('portfolio/contact', { title: 'CONTACT' });
+});
+
+router.get('/portfolio/projects', async(req, res, next) => {
+  res.render('portfolio/projects', { title: 'PROJECTS' });
+});
+
+
 module.exports = router;
